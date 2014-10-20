64294764b56cf3f55bdcc245e71253d2bb9c624c
Joey De La Torre <jdcrewdawg@Joeys-iMac.local>
2014-10-19 23:58:58 -0400

    Assignment 1 part 1 and 2

--- complete.R
+++ complete.R
@@ -1,4 +1,12 @@
 complete <- function(directory, id = 1:332) {
         files_list <- list.files(directory, full.names = TRUE)
-        
+        nobs <- vector()
+        for (i in id) {
+                id_names <- c(id)
+                nobs <- c(nobs, sum(complete.cases(read.csv(files_list[i]))))
+                
+        }
+        dat <- cbind(id_names, nobs)
+        colnames(dat) <- c("id", "nobs")
+        return(data.frame(dat))
 }
